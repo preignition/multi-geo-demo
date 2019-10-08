af21eb2e37aa56920997a28b8064c61844b9592e
christophe-g <christophe.geiser@gmail.com>
2019-10-08 14:57:37 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Multi Charts Demo
+Basic examples for working with `lit-html` and `LitElement`. 
+
+See the `./01-basic/01-basic-setup.js` to get started.
---
+++ es-dev-server.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  port: 8081,
+  watch: true,
+  nodeResolve: true,
+    preserveSymlinks: true,
+  // rootDir: '../',
+  // basePathr: '/demo',
+  // appIndex: 'demo/index.html'
+}
